869d89b56289d3fc5ffc5c8b2902c908bb43a67e
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2021-05-25 19:00:08 -0500

    valores iniciales obtenidos de la gráfica

--- diferential_cross_section.cpp
+++ diferential_cross_section.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <vector>
 #define _USE_MATH_DEFINES
 
 const double Q=0.292; //GeV2
 const double alpha = 1.0/137;
 const double hbarc = 1.97327; // 10^(-14)GeV cm
+const int N_data = 5;
 
 double scattered_electron_energy (double E1, int theta);
 double diferential_cross_section_Mott (double E1, double E3, int theta);
+void initial_values (std::vector<int>& angles, std::vector<double> & energies1, std::vector<double> & energies3, std::vector<double> & expdifcrosection);
 
 int main ()
 {
   std::cout.precision(6);
   std::cout.setf(std::ios::scientific);
+
+  std::vector<int>angles(N_data, 0);
+  std::vector<double>energies1(N_data, 0.0);
+  std::vector<double>energies3(N_data, 0.0);
+  std::vector<double>expdifcrosection(N_data, 0.0);
   
   double E1 = 0.5; //GeV
   double E3 = 0.0; //GeV
@@ -21,7 +29,7 @@ int main ()
   int theta = 90;
   E3 = scattered_electron_energy (E1, theta);
     
-  std::cout<<E3<<"\t"<<diferential_cross_section_Mott(E1, E3, theta)<<std::endl;
+  std::cout<<E3<<"\t"<<diferential_cross_section_Mott(E1, E3, theta)<<"\t"<<energies1[2]<<"\t"<<angles[2]<<"\t"<<std::endl;
   
   return 0;
 }
@@ -45,7 +53,41 @@ double diferential_cross_section_Mott (double E1, double E3, int angle)
 
   //Calculating the Mott differential cross section from natural units to SI:
 
-  Mott = (Mott/4*M_PI)*hbarc*hbarc*pow(10, -28);  //Mott en cm2/steradian
+  Mott = (Mott/4*M_PI)*hbarc*hbarc*pow(10, -28)/10;  //Mott en cm2/steradian with correction of the pow function on 10
   
   return Mott;
+}
+
+void initial_values (std::vector<int>& angles, std::vector<double> & energies1, std::vector<double> & energies3, std::vector<double> & expdifcrosection)
+{
+  //angles values
+  angles[0] = 60;
+  angles[1] = 75;
+  angles[2] = 90;
+  angles[3] = 120;
+  angles[4] = 135;
+
+  //energies of incident electron
+
+  energies1[0] = 0.0;
+  energies1[1] = 0.0;
+  energies1[2] = 0.0;
+  energies1[3] = 0.0;
+  energies1[4] = 0.0;
+
+  //energies of scattered electron
+
+  energies3[0] = 0.0;
+  energies3[1] = 0.0;
+  energies3[2] = 0.0;
+  energies3[3] = 0.0;
+  energies3[4] = 0.0;
+
+  //experimental differential cross section
+
+  expdifcrosection[0] = 0.0;
+  expdifcrosection[0] = 0.0;
+  expdifcrosection[0] = 0.0;
+  expdifcrosection[0] = 0.0;
+  expdifcrosection[0] = 0.0;
 }
